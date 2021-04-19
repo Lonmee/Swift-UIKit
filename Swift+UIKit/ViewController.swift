739e82857b3b23b19e8054e0be9f71970f12512f
Lonmee <Lonmee@126.com>
2021-04-20 03:25:31 +0800

    x

--- Swift+UIKit/ViewController.swift
+++ Swift+UIKit/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    var imagePicker: UIImagePickerController!
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func PressHandler(_ sender: Any) {
+        if self.imagePicker == nil {
+            self.imagePicker = UIImagePickerController()
+        }
+        self.imagePicker.delegate = self
+        self.imagePicker.sourceType = .photoLibrary
+        self.present(self.imagePicker, animated: true, completion: nil)
+        
+        //        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+        //            if self.imagePicker == nil{
+        //                self.imagePicker = UIImagePickerController()
+        //            }
+        //            self.imagePicker.delegate = self
+        //            self.imagePicker.sourceType = .camera
+        //            self.present(self.imagePicker, animated: true, completion: nil)
+        //        }
+        //        else{
+        //            let errorAlert = UIAlertController(title: "相机不可用", message: "", preferredStyle: UIAlertController.Style.alert)
+        //            let cancelAction = UIAlertAction(title: "确定", style: UIAlertAction.Style.cancel, handler: nil)
+        //            errorAlert.addAction(cancelAction)
+        //            self.present(errorAlert, animated: true, completion: nil)
+        //        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.imagePicker = nil
+        self.dismiss(animated: true, completion: nil)
+        print("dismiss")
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey(rawValue: UIImagePickerController.InfoKey.originalImage.rawValue)] as! UIImage
+        self.imageView.image = image
+        self.imageView.contentMode = .scaleAspectFill
+        self.imagePicker.delegate = nil
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
